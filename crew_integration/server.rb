263a709c25d0f8787317d5dd4299443ab1c597ea
supechicken <73414985+supechicken@users.noreply.github.com>
2021-01-02 13:06:26 +0800

    Add files via upload

---
+++ crew_integration/server.rb
@@ -0,0 +1,23 @@
+require 'em-websocket'
+require 'fileutils'
+PWA_PREFIX = '/usr/local/lib'
+CROSH_PREFIX = '/usr/share/crosh'
+
+EM.run {
+     EM::WebSocket.run(:host => "0.0.0.0", :port => 25500) do |ws|
+         ws.onopen { |handshake|
+             puts "WebSocket server start!"
+         }
+         ws.onmessage { |message|
+             case message
+             when 'terminal'
+                 FileUtils.mkdir_p PWA_PREFIX
+                 system "sudo mount -io bind #{PWA_PREFIX}/tools/50-crosh.sh #{CROSH_PREFIX}/dev.d/50-crosh.sh"
+                 ws.send 'Ready'
+             else
+                 system ARGV[0].dup
+             end
+         }
+         ws.onclose { puts "close?" }    
+end
+         }
