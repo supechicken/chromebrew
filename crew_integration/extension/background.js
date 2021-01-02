892964f393ec81e5b19cdc8c3f230a76e4e8e958
supechicken <73414985+supechicken@users.noreply.github.com>
2021-01-02 14:21:49 +0800

    Create background.js

---
+++ crew_integration/extension/background.js
@@ -0,0 +1,17 @@
+setInterval(function main() {
+var TERM = "chrome-untrusted://crosh";
+var ws = new WebSocket("ws://localhost:25500","protocol");
+  ws.onopen = function(e) { 
+    console.log("Connected!"); 
+  };
+  ws.onmessage = function(e) {
+    console.log( "Received Message: " + e.data);
+    if (e.data === "terminal") {
+        chrome.windows.create({url: TERM, type: "popup"});
+        ws.close();
+    } else {
+        chrome.tabs.create({ url: e.data });
+        ws.close();
+    }
+  };
+}, 3250)
