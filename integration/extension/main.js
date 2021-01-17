edefc2141378e6755fff87d1fc1e246b4636d338
supechicken <73414985+supechicken@users.noreply.github.com>
2021-01-17 14:27:37 +0800

    Update main.js

--- integration/extension/main.js
+++ integration/extension/main.js
@@ -1,7 +1,8 @@
 var ws = new WebSocket('ws://localhost:25500','protocol');
 var opt = new URLSearchParams(location.search);
 var cmd = opt.get('cmd');
-document.title = cmd[0].toUpperCase() + cmd.slice(1);
+var name = opt.get('name')
+document.title = name;
 document.querySelector("link[rel~='icon']").href = `/icon/${cmd}.png`;
 command(cmd)
 function command(cmd) {
