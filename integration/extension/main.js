222d29b0cd56987121f869ade381e8636be16b76
supechicken <73414985+supechicken@users.noreply.github.com>
2021-01-17 13:24:04 +0800

    Update main.js

--- integration/extension/main.js
+++ integration/extension/main.js
@@ -3,10 +3,9 @@ var cmd = opt.get('cmd');
 document.title = cmd[0].toUpperCase() + cmd.slice(1);
 document.querySelector("link[rel~='icon']").href = `/icon/${cmd}.png`;
 command(cmd)
-
 function command(cmd) {
     if (cmd === 'terminal') {
-        chrome.windows.open('/html/crosh.html');
+        chrome.windows.create({url: '/html/crosh.html'});
         self.close()
     } else {
         var ws = new WebSocket('ws://localhost:25500','protocol');
