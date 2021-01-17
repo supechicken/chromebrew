f4a81f3661b08b49226af6ad16e128db40fe0da1
supechicken <73414985+supechicken@users.noreply.github.com>
2021-01-17 13:23:17 +0800

    Update crosh.js

--- integration/extension/js/crosh.js
+++ integration/extension/js/crosh.js
@@ -2,9 +2,8 @@ var NEWTERM = 'chrome-untrusted://crosh/',
     OLDTERM = 'chrome-extension://nkoccljplnhpfnfiajclkommnmllphnl/html/crosh.html';
 chrome.runtime.sendMessage('nkoccljplnhpfnfiajclkommnmllphnl', null, null, (response) => {
     if (chrome.runtime.lastError) {
-        var TERM = NEWTERM
+        chrome.tabs.update({url: NEWTERM})
     } else {
-        var TERM = OLDTERM
+        chrome.tabs.update({url: OLDTERM})
     }
-};
-chrome.tabs.update(TERM)
+})
