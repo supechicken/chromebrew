b6ba5cf368144eb5a23a830eafc86a45fdf8224b
supechicken <supechicken666@gmail.com>
2022-09-23 22:25:56 +0800

    Update const.rb

--- lib/const.rb
+++ lib/const.rb
@@ -6,15 +6,15 @@ require_relative 'color'
 # import config file (in #{CREW_PREFIX}/etc/crew/config.json)
 config_path = File.expand_path('../../../etc/crew/config.json', __dir__)
 
-if File.exist?(config_path)
-  JSON.load_file(config_path).each_pair do |name, value|
-    value = eval(value.inspect.sub('\#{', '#{')) # resolve variables in string
-    Object.const_set(name, value)
-  end
-else
+unless File.exist?(config_path)
   # generate a new one if not exist
   warn 'Configuration file does not exist! A new one will be generated.'.yellow
   system File.join(__dir__, '../bin/crew_generate_config')
+end
+
+JSON.load_file(config_path).each_pair do |name, value|
+  value = eval(value.inspect.sub('\#{', '#{')) # resolve variables in string
+  Object.const_set(name, value)
 end
 
 # import runtime constants (in lib/runtime_const.rb)
