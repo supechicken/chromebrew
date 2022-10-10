dc575c50f5c7d3bf637fa42a1e62cfb5b54bff55
supechicken <supechicken666@gmail.com>
2022-10-11 02:39:21 +0800

    Fix typo

--- packages/dropbox_uploader.rb
+++ packages/dropbox_uploader.rb
@@ -4,7 +4,7 @@ class Dropbox_uploader < Package
   description 'Dropbox Uploader is a BASH script which can be used to upload, download, list or delete files from Dropbox, an online file sharing, synchronization and backup service.'
   homepage 'https://github.com/andreafabrizi/Dropbox-Uploader'
   @_commit = '11fb8f736064730dd21ff85d68dfcc8aacfdf559'
-  version "1.0+#{commit[0, 7]}"
+  version "1.0+#{@_commit[0, 7]}"
   compatibility 'all'
   license 'GPL-3'
 
