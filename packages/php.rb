e93a813452b7c7ee15a8b4c171d5b388bcb197ef
supechicken <supechicken666@gmail.com>
2022-10-16 17:22:56 +0800

    php*: Rubyize

--- packages/php.rb
+++ packages/php.rb
@@ -2,60 +2,41 @@ require 'package'
 
 class Php < Package
   description 'PHP is a popular general-purpose scripting language that is especially suited to web development.'
-  homepage 'http://www.php.net/'
-  version '5.6.40-8.1.11'
+  homepage 'https://www.php.net'
   license 'PHP-3.01'
   compatibility 'all'
+
+  @avail_php_ver = Dir["#{CREW_PACKAGES_PATH}/php?*.rb"].map do |pkgFile|
+    php_majver = pkgFile[/php(\d+).rb/, 1].to_i
+    pkg        = Package.load_package(pkgFile)
+
+    [php_majver, pkg.version]
+  end.sort_by do |(php_majver, _)|
+    php_majver
+  end.to_h
+
+  version "#{@avail_php_ver.values[0]}-#{@avail_php_ver.values[-1]}"
 
   is_fake
 
   def self.preflight
-    major = `php -v 2> /dev/null | head -1 | cut -d' ' -f2 | cut -d'.' -f1`.chomp
-    minor = `php -v 2> /dev/null | head -1 | cut -d' ' -f2 | cut -d'.' -f2`.chomp
-    unless major.empty? && minor.empty?
-      puts "Php#{major}#{minor} already installed.".lightgreen
-      abort "Enter `crew remove php#{major}#{minor} && crew install php` to install a different version."
-    end
-    puts
-    puts 'Select version:'
-    puts '5.6 = PHP 5.6.40'
-    puts '7.1 = PHP 7.1.33'
-    puts '7.2 = PHP 7.2.34'
-    puts '7.3 = PHP 7.3.33'
-    puts '7.4 = PHP 7.4.32'
-    puts '8.0 = PHP 8.0.24'
-    puts '8.1 = PHP 8.1.11'
-    puts '  0 = Cancel'
+    if ARGV.include?('install')
+      php_exec = File.join(CREW_PREFIX, 'bin', 'php')
 
-    while version = $stdin.gets.chomp
-      case version
-      when '5.6'
-        depends_on 'php5'
-        break
-      when '7.1'
-        depends_on 'php71'
-        break
-      when '7.2'
-        depends_on 'php72'
-        break
-      when '7.3'
-        depends_on 'php73'
-        break
-      when '7.4'
-        depends_on 'php74'
-        break
-      when '8.0'
-        depends_on 'php80'
-        break
-      when '8.1'
-        depends_on 'php81'
-        break
-      when '0'
-        abort
-        break
-      else
-        puts "\nPlease select from one of the options or enter 0 to cancel."
+      if File.exist?(php_exec)
+        full_ver                = `#{php_exec} -v`[/^PHP ([^\s]+)/, 1]
+        major_ver, minor_ver, _ = full_ver.split('.', 3)
+
+        abort <<~EOT.yellow
+
+          PHP version #{full_ver} installed.
+
+          Run "crew remove php#{major_ver}#{minor_ver}; crew install php" to install another version of PHP
+        EOT
       end
+
+      options = @avail_php_ver.map {|majver, ver| { value: "php#{majver}", description: "PHP #{ver}" } }
+      depends_on Selector.new(options).show_prompt
     end
   end
 end
--- packages/php72.rb
+++ packages/php72.rb
@@ -11,96 +11,100 @@ class Php72 < Package
   source_sha256 '409e11bc6a2c18707dfc44bc61c820ddfd81e17481470f3405ee7822d8379903'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/php72/7.2.34-2_armv7l/php72-7.2.34-2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/php72/7.2.34-2_armv7l/php72-7.2.34-2-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/php72/7.2.34-2_i686/php72-7.2.34-2-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/php72/7.2.34-2_x86_64/php72-7.2.34-2-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: 'aa82eeacd83183c4efe99abb1f3b75601dba24eecea68a57a8a37613a3dc2614',
-     armv7l: 'aa82eeacd83183c4efe99abb1f3b75601dba24eecea68a57a8a37613a3dc2614',
-       i686: '5d0770534aff60ecb57d99b1ed8ecf2b13058a06f7056162541c1e370dbb1334',
-     x86_64: '40c16a7e5635c5c2a9bd6f41b9db894237147c02d6c5b8d7175c0a97639eea5c'
   })
 
   depends_on 'libgcrypt'
-  depends_on 'libiconv'
-  depends_on 'libjpeg'
-  depends_on 'libxslt'
+  depends_on 'libpng'
   depends_on 'libzip'
-  depends_on 'libcurl'
   depends_on 'exif'
   depends_on 'freetype'
   depends_on 'pcre'
   depends_on 're2c'
-  depends_on 'tidy'
-  depends_on 'unixodbc'
+
+  depends_on 'libcurl' => :build
+  depends_on 'libxslt' => :build
+  depends_on 'tidy' => :build
+  depends_on 'unixodbc' => :build
+
+  no_fhs
 
   def self.preflight
-    phpver = `php -v 2> /dev/null | head -1 | cut -d' ' -f2`.chomp
-    abort "PHP version #{phpver} already installed.".lightgreen if ARGV[0] != 'reinstall' && @_ver != phpver && !phpver.empty?
+    php_exec = File.join(CREW_PREFIX, 'bin', 'php')
+
+    if File.exist?(php_exec)
+      full_ver                = `#{php_exec} -v`[/^PHP ([^\s]+)/, 1]
+      major_ver, minor_ver, _ = full_ver.split('.', 3)
+
+      abort <<~EOT.yellow unless name == "php#{major_ver}#{minor_ver}"
+
+        PHP version #{full_ver} installed.
+
+        Run "crew remove php#{major_ver.eql?('5') ? '5' : "#{major_ver}#{minor_ver}"}; crew install #{name}" to install this version of PHP
+      EOT
+    end
   end
 
   def self.patch
     # Configuration
-    system "sed -i 's,;pid = run/php-fpm.pid,pid = #{CREW_PREFIX}/tmp/run/php-fpm.pid,' sapi/fpm/php-fpm.conf.in"
-    system "sed -i 's,;error_log = log/php-fpm.log,error_log = #{CREW_PREFIX}/log/php-fpm.log,' sapi/fpm/php-fpm.conf.in"
-    system "sed -i 's,include=@php_fpm_sysconfdir@/php-fpm.d,include=#{CREW_PREFIX}/etc/php-fpm.d,' sapi/fpm/php-fpm.conf.in"
-    system "sed -i 's,^user,;user,' sapi/fpm/www.conf.in"
-    system "sed -i 's,^group,;group,' sapi/fpm/www.conf.in"
-    system "sed -i 's,@sbindir@,#{CREW_PREFIX}/bin,' sapi/fpm/init.d.php-fpm.in"
-    system "sed -i 's,@sysconfdir@,#{CREW_PREFIX}/etc,' sapi/fpm/init.d.php-fpm.in"
-    system "sed -i 's,@localstatedir@,#{CREW_PREFIX}/tmp,' sapi/fpm/init.d.php-fpm.in"
-    # Set some sane defaults
-    system "sed -i 's,post_max_size = 8M,post_max_size = 128M,' php.ini-development"
-    system "sed -i 's,upload_max_filesize = 2M,upload_max_filesize = 128M,' php.ini-development"
-    system "sed -i 's,;opcache.enable=0,opcache.enable=1,' php.ini-development"
-    # Fix cc: error: ext/standard/.libs/type.o: No such file or directory
-    # system "sed -i '98303d' configure"
-    # system "sed -i '98295,98296d' configure"
+    File.open('sapi/fpm/php-fpm.conf.in', 'r+') do |fileIO|
+      fileIO.write fileIO.read \
+        .sub(';pid = run/php-fpm.pid', "pid = #{CREW_PREFIX}/tmp/run/php-fpm.pid") \
+        .sub(';error_log = log/php-fpm.log', "error_log = #{CREW_PREFIX}/log/php-fpm.log") \
+        .sub('include=@php_fpm_sysconfdir@/php-fpm.d', "include=#{CREW_PREFIX}/etc/php-fpm.d") \
+        .sub('^user', ';user') \
+        .sub('^group', ';group') \
+        .sub('@sbindir@', "#{CREW_PREFIX}/bin") \
+        .sub('@sysconfdir@', "#{CREW_PREFIX}/etc") \
+        .sub('@localstatedir@', "#{CREW_PREFIX}/tmp")
+    end
+
+    system 'sed', '-i', 's,^user,;user,', 'sapi/fpm/www.conf.in'
+    system 'sed', '-i', 's,^group,;group,', 'sapi/fpm/www.conf.in'
+
     # Fix /usr/bin/file: No such file or directory
     system 'filefix'
   end
 
   def self.build
-    ENV['CFLAGS'] = ' -liconv'
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--docdir=#{CREW_PREFIX}/doc",
-           "--infodir=#{CREW_PREFIX}/info",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           "--localstatedir=#{CREW_PREFIX}/tmp",
-           "--mandir=#{CREW_PREFIX}/share/man",
-           "--sbindir=#{CREW_PREFIX}/bin",
-           "--with-config-file-path=#{CREW_PREFIX}/etc",
-           "--with-libdir=#{ARCH_LIB}",
-           "--with-kerberos=#{CREW_LIB_PREFIX}",
-           '--enable-exif',
-           '--enable-fpm',
-           '--enable-ftp',
-           '--enable-mbstring',
-           '--enable-opcache',
-           '--enable-pcntl',
-           '--enable-shared',
-           '--enable-shmop',
-           '--enable-sockets',
-           '--enable-zip',
-           '--with-bz2',
-           '--with-curl',
-           '--with-gd',
-           '--with-gettext',
-           '--with-gmp',
-           '--with-libzip',
-           '--with-mysqli',
-           '--with-openssl',
-           '--with-pcre-regex',
-           '--with-pdo-mysql',
-           '--with-pear',
-           '--with-readline',
-           '--with-tidy',
-           '--with-unixODBC',
-           '--with-xsl',
-           '--with-zlib'
+    system <<~BUILD
+      ./configure #{CREW_OPTIONS} \
+        --docdir=#{CREW_PREFIX}/doc \
+        --infodir=#{CREW_PREFIX}/info \
+        --localstatedir=#{CREW_PREFIX}/tmp \
+        --sbindir=#{CREW_PREFIX}/bin \
+        --with-config-file-path=#{CREW_PREFIX}/etc \
+        --with-libdir=#{ARCH_LIB} \
+        --with-kerberos=#{CREW_LIB_PREFIX} \
+        --enable-exif \
+        --enable-fpm \
+        --enable-ftp \
+        --enable-mbstring \
+        --enable-opcache \
+        --enable-pcntl \
+        --enable-shared \
+        --enable-shmop \
+        --enable-sockets \
+        --enable-zip \
+        --with-bz2 \
+        --with-curl \
+        --with-gd \
+        --with-gettext \
+        --with-gmp \
+        --with-libzip \
+        --with-mysqli \
+        --with-openssl \
+        --with-pcre-regex \
+        --with-pdo-mysql \
+        --with-pear \
+        --with-readline \
+        --with-tidy \
+        --with-unixODBC \
+        --with-xsl \
+        --with-zlib
+    BUILD
+
     system 'make'
   end
 
@@ -109,40 +113,50 @@ class Php72 < Package
   end
 
   def self.install
-    ENV['CREW_FHS_NONCOMPLIANCE_ONLY_ADVISORY'] = '1'
-    reload_constants
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/log"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/tmp/run"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/init.d"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/php-fpm.d"
+    FileUtils.mkdir_p %W[#{CREW_DEST_PREFIX}/log #{CREW_DEST_PREFIX}/tmp/run #{CREW_DEST_PREFIX}/etc/init.d #{CREW_DEST_PREFIX}/etc/php-fpm.d]
+
     system 'make', "INSTALL_ROOT=#{CREW_DEST_DIR}", 'install'
+
     FileUtils.install 'php.ini-development', "#{CREW_DEST_PREFIX}/etc/php.ini", mode: 0o644
     FileUtils.install 'sapi/fpm/init.d.php-fpm.in', "#{CREW_DEST_PREFIX}/etc/init.d/php-fpm", mode: 0o755
     FileUtils.install 'sapi/fpm/php-fpm.conf.in', "#{CREW_DEST_PREFIX}/etc/php-fpm.conf", mode: 0o644
     FileUtils.install 'sapi/fpm/www.conf.in', "#{CREW_DEST_PREFIX}/etc/php-fpm.d/www.conf", mode: 0o644
+
     FileUtils.ln_s "#{CREW_PREFIX}/etc/init.d/php-fpm", "#{CREW_DEST_PREFIX}/bin/php7-fpm"
 
     # clean up some files created under #{CREW_DEST_DIR}. check http://pear.php.net/bugs/bug.php?id=20383 for more details
-    FileUtils.mv "#{CREW_DEST_DIR}/.depdb", "#{CREW_DEST_LIB_PREFIX}/php"
-    FileUtils.mv "#{CREW_DEST_DIR}/.depdblock", "#{CREW_DEST_LIB_PREFIX}/php"
-    FileUtils.rm_rf ["#{CREW_DEST_DIR}/.channels", "#{CREW_DEST_DIR}/.filemap", "#{CREW_DEST_DIR}/.lock", "#{CREW_DEST_DIR}/.registry"]
+    FileUtils.mv %W[#{CREW_DEST_DIR}/.depdb #{CREW_DEST_DIR}/.depdblock], "#{CREW_DEST_LIB_PREFIX}/php"
+    FileUtils.rm_rf Dir["#{CREW_DEST_DIR}/.{channels,filemap,lock,registry}"]
   end
 
   def self.postinstall
-    puts
-    puts 'To start the php-fpm service, execute:'.lightblue
-    puts 'php7-fpm start'.lightblue
-    puts
-    puts 'To stop the php-fpm service, execute:'.lightblue
-    puts 'php7-fpm stop'.lightblue
-    puts
-    puts 'To restart the php-fpm service, execute:'.lightblue
-    puts 'php7-fpm restart'.lightblue
-    puts
-    puts 'To start php-fpm on login, execute the following:'.lightblue
-    puts "echo 'if [ -f #{CREW_PREFIX}/bin/php7-fpm ]; then' >> ~/.bashrc".lightblue
-    puts "echo '  #{CREW_PREFIX}/bin/php7-fpm start' >> ~/.bashrc".lightblue
-    puts "echo 'fi' >> ~/.bashrc".lightblue
-    puts 'source ~/.bashrc'.lightblue
+    print "Start php-fpm on login? [Y/n]: "
+
+    if %W[Y y \n].include?($stdin.getc)
+      File.write "#{CREW_PREFIX}/etc/env.d/#{name}", <<~EOF
+        # start php-fpm on login
+        if [ -e #{CREW_PREFIX}/bin/php7-fpm ]; then
+          #{CREW_PREFIX}/bin/php7-fpm start
+        fi
+      EOF
+    end
+
+    puts <<~EOT.lightblue
+
+      To start the php-fpm service, execute:
+      php7-fpm start
+
+      To stop the php-fpm service, execute:
+      php7-fpm stop
+
+      To restart the php-fpm service, execute:
+      php7-fpm restart
+
+    EOT
+  end
+
+  def self.remove
+    # remove env file if exist
+    FileUtils.rm_f "#{CREW_PREFIX}/etc/env.d/#{name}"
   end
 end
