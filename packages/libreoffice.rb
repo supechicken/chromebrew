885485c30d61bb03381ec2d10d6da13f4f66c381
supechicken <supechicken666@gmail.com>
2022-12-21 18:33:46 +0800

    Fix typo

--- packages/libreoffice.rb
+++ packages/libreoffice.rb
@@ -2,13 +2,13 @@ require 'package'
 
 class Libreoffice < Package
   description ''
-  homepage ''
+  homepage 'https://www.libreoffice.org/'
   license ''
   version '7.4.4.1'
   compatibility 'all'
 
   source_url 'https://gerrit.libreoffice.org/core.git'
-  git_branch 'libreoffice-7.4.4.1'
+  git_hashtag 'libreoffice-7.4.4.1'
 
   depends_on 'gtk3'
   depends_on 'hunspell'
