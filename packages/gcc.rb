689b01c3d4b1e5121d1f16c621be9fb0dc0c2708
supechicken <supechicken666@gmail.com>
2022-12-25 15:30:10 +0800

    Only enable default languages

--- packages/gcc.rb
+++ packages/gcc.rb
@@ -98,7 +98,7 @@ class Gcc < Package
       BUILD
     end
 
-    @languages = 'c,c++,d,fortran,go,jit,lto,m2,objc,obj-c++'
+    @languages = 'default'
 
     # Set ccache sloppiness as per
     # https://wiki.archlinux.org/index.php/Ccache#Sloppiness
