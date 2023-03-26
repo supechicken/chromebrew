8621bae12aa45741af3c6829514295633fdf46ce
supechicken <supechicken666@gmail.com>
2023-03-26 09:02:40 +0800

    elixir => 1.14.3, erlang => 25.3

--- packages/elixir.rb
+++ packages/elixir.rb
@@ -3,28 +3,23 @@ require 'package'
 class Elixir < Package
   description 'Elixir is a dynamic, functional language designed for building scalable and maintainable applications.'
   homepage 'https://elixir-lang.org/'
-  version '1.13.4'
+  version '1.14.3'
   license 'Apache-2.0 and ErlPL-1.1'
   compatibility 'all'
-  source_url 'https://github.com/elixir-lang/elixir/releases/download/v1.13.4/Precompiled.zip'
-  source_sha256 'e74eee9d4e1c64e7add0e2b4e2fb41aaaa7450522f46f8e922b9bf6a27587606'
+  source_url 'https://github.com/elixir-lang/elixir/archive/refs/tags/v1.14.3.tar.gz'
+  source_sha256 'bd464145257f36bd64f7ba8bed93b6499c50571b415c491b20267d27d7035707'
 
   depends_on 'erlang'
 
-  no_compile_needed
+  def self.build
+    system 'make'
+  end
+
+  def self.test
+    system 'make', 'test'
+  end
 
   def self.install
-    # Remove unused files
-    FileUtils.rm_f 'man/common'
-    FileUtils.rm_f 'bin/mix.ps1'
-    FileUtils.rm_f Dir['bin/*.bat']
-    FileUtils.rm_f Dir['man/*.1.in']
-    # Prepare destination directories
-    FileUtils.mkdir_p CREW_DEST_LIB_PREFIX.to_s
-    FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/man1"
-    # Add relevant files
-    FileUtils.cp_r 'bin/', CREW_DEST_PREFIX.to_s
-    FileUtils.cp_r Dir['lib/mix/lib/*'], CREW_DEST_LIB_PREFIX.to_s
-    FileUtils.cp_r Dir['man/*'], "#{CREW_DEST_MAN_PREFIX}/man1"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
--- packages/erlang.rb
+++ packages/erlang.rb
@@ -3,31 +3,18 @@ require 'package'
 class Erlang < Package
   description 'Erlang is a programming language used to build massively scalable soft real-time systems with requirements on high availability.'
   homepage 'https://www.erlang.org/'
-  version '25.1.2'
+  version '25.3'
   license 'Apache-2.0'
   compatibility 'all'
-  source_url 'https://github.com/erlang/otp/releases/download/OTP-25.1.2/otp_src_25.1.2.tar.gz'
-  source_sha256 '5442dea694e7555d479d80bc81f1428020639c258f8e40b2052732d1cc95cca5'
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/erlang/25.1.2_armv7l/erlang-25.1.2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/erlang/25.1.2_armv7l/erlang-25.1.2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/erlang/25.1.2_i686/erlang-25.1.2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/erlang/25.1.2_x86_64/erlang-25.1.2-chromeos-x86_64.tar.zst'
-  })
-  binary_sha256({
-    aarch64: '5d11d08a183a483705243ac119c22c5beda2198a379680c70cadb6fab5e178bb',
-     armv7l: '5d11d08a183a483705243ac119c22c5beda2198a379680c70cadb6fab5e178bb',
-       i686: '3ee3bc29cf2bcaa42847c65f050ef66af8f699e12d1afdfc1532d27b3268ae7a',
-     x86_64: '22d0017e5f52884785b60ee4b866ef357b54e416e2162f7747b82778a0aa703a'
-  })
+  source_url 'https://github.com/erlang/otp/releases/download/OTP-25.3/otp_src_25.3.tar.gz'
+  source_sha256 '85c447efc1746740df4089d75bc0e47b88d5161d7c44e9fc4c20fa33ea5d19d7'
 
   depends_on 'openjdk8'
   depends_on 'wxwidgets'
 
   def self.build
-    ENV['ERL_OTP'] = Dir.pwd
-    system "./configure #{CREW_OPTIONS} && make"
+    system "./configure #{CREW_OPTIONS}"
+    system 'make'
   end
 
   def self.install
