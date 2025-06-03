47be55472369b8b24d360caafe17be5e9ea37f24
SupeChicken666 <me@supechicken666.dev>
2025-06-04 00:26:21 +0800

    not global
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ echo_out 'Set up the local package repo...'
   cd "${CREW_LIB_PATH}"
 
   # Make the git default branch error messages go away.
-  git config --global init.defaultBranch main
+  git config init.defaultBranch main
 
   # Setup the folder with git information.
   git init --ref-format=reftable
