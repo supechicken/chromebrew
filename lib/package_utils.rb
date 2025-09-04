8d7c40e054657e7a459823ebf4fc63f171d36ff9
SupeChicken666 <supechicken666@gmail.com>
2025-09-04 05:59:11 +0000

    test
    Signed-off-by: SupeChicken666 <supechicken666@gmail.com>

--- lib/package_utils.rb
+++ lib/package_utils.rb
@@ -15,13 +15,17 @@ class PackageUtils
         pkg_file      = File.join(CREW_PACKAGES_PATH, "#{installed_pkg_info[:name]}.rb")
         installed_pkg = Package.load_package(pkg_file)
 
-        pkgs_that_need_it << installed_pkg.name if installed_pkg.dependencies.key?(dep_name) && !pkg.dependencies[dep_name][0].include?(:build)
+        if installed_pkg.dependencies.key?(dep_name) && !installed_pkg.dependencies[dep_name][0].include?(:build)
+          pkgs_that_need_it << installed_pkg.name
+        end
       end
     else
       Dir.glob("#{CREW_PACKAGES_PATH}/*.rb") do |pkg_file|
         pkg = Package.load_package(pkg_file)
 
-        pkgs_that_need_it << pkg.name if pkg.dependencies.key?(dep_name) && !pkg.dependencies[dep_name][0].include?(:build)
+        if pkg.dependencies.key?(dep_name) && !pkg.dependencies[dep_name][0].include?(:build)
+          pkgs_that_need_it << pkg.name
+        end
       end
     end
 
